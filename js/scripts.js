f756cec511ce9baad4da489a624a713a7d25c91a
ashrazaman <73212617+ashrazaman@users.noreply.github.com>
2021-11-13 09:54:02 +0600

    added slider

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+const swiper = new Swiper('.brands-slider, .shops-slider', {
+  loop: true,
+  slidesPerView: 6,
+  spaceBetween: 28,
+  autoplay: {
+   delay: 1500,
+  },
+  /*
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  },
+*/
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});

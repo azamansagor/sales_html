e647cfcebac771e822b9877424be98fc9cb18c9a
ashrazaman <73212617+ashrazaman@users.noreply.github.com>
2021-11-24 20:08:20 +0600

    responsive and menu update

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,36 @@ const swiper = new Swiper('.brands-slider, .shops-slider', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function categoryMenuFunction() {
+  document.getElementById("categoryDropdown").classList.toggle("show");
+  document.getElementsByTagName( 'body' )[0].classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-button')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+function openNav() {
+  document.getElementById("mobile-menu").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mobile-menu").style.width = "0";
+}

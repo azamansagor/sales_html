098ad270937cf651e89dee2d687736e25f4fd9b5
ashrazaman <73212617+ashrazaman@users.noreply.github.com>
2022-01-17 00:50:33 +0600

    single product page

--- js/scripts.js
+++ js/scripts.js
@@ -134,6 +134,43 @@ new Swiper('.category-thumbs-slider', {
   },
 });
 
+/* single product page slider */
+var galleryThumbs = new Swiper(".gallery-thumbs", {
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+  slidesPerView: 3,
+  watchOverflow: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  direction: 'vertical'
+});
+
+var galleryMain = new Swiper(".gallery-main", {
+  watchOverflow: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  preventInteractionOnTransition: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  effect: 'fade',
+    fadeEffect: {
+    crossFade: true
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
+});
+
+galleryMain.on('slideChangeTransitionStart', function() {
+  galleryThumbs.slideTo(galleryMain.activeIndex);
+});
+
+galleryThumbs.on('transitionStart', function(){
+  galleryMain.slideTo(galleryThumbs.activeIndex);
+});
+
 /* header category dropdown functionality start */
 function categoryMenuFunction() {
   document.getElementById("categoryDropdown").classList.toggle("show");
